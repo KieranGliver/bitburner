45cef703c2c879fd3466f7b7763df9e261bd60ad
KieranGliver <kieran.glovjunk@gmail.com>
2025-04-06 16:47:40 -0400

    Removed old files

--- src/old-files/test.ts
+++
@@ -1,7 +0,0 @@
-import { NS } from "@ns";
-import { countTotalThreads, displayUI, getServerList, runScript } from "./functions";
-
-export async function main(ns: NS): Promise<void> {
-    const servers = getServerList(ns);
-    await runScript(ns, servers, "grow.js", 10, ns.args[0])
-}
