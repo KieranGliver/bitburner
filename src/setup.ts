9abab0ab075a42c56ff6a76d1b4e493fc98ab924
KieranGliver <kieran.glovjunk@gmail.com>
2025-04-07 21:18:51 -0400

    removed running server-hack in setup

--- src/setup.ts
+++ src/setup.ts
@@ -4,6 +4,12 @@ import { crackServer, getServerList, runScript } from './utils';
 
 // Total scirpt ram cost should be under 8GB to allow to run on new saves
 export async function main(ns: NS): Promise<void> {
+    const isHackable = (hostname: string) =>
+		hostname !== 'home' &&
+		ns.getServerRequiredHackingLevel(hostname) <= ns.getHackingLevel() &&
+		ns.getServerMaxMoney(hostname) > 0 &&
+		ns.hasRootAccess(hostname);
+    
 	const serverList = getServerList(ns);
 	const crackServers = serverList.filter(
 		(hostname) =>
@@ -22,13 +28,13 @@ export async function main(ns: NS): Promise<void> {
 
 	const serverDisplayPid = runScript(ns, serverList, 'server-display.js', 1);
 	if (serverDisplayPid.length === 0) {
-		ns.print('ERROR: server-manager.js failed to start');
+		ns.tprint('ERROR: server-manager.js failed to start');
 	}
 
 	const serverManagerPid = runScript(ns, serverList, 'server-manager.js', 1);
 
 	if (serverManagerPid.length === 0) {
-		ns.print('ERROR: server-manager.js failed to start');
+		ns.tprint('ERROR: server-manager.js failed to start');
 	}
 
 	const hackServers = crackServers.filter(
@@ -37,22 +43,15 @@ export async function main(ns: NS): Promise<void> {
 	);
 
 	// run server-hack.js on all valid servers
-	/*
-	for (const hostname of hackServers) {
+    /*
+	for (const hostname of hackServers.filter(isHackable)) {
 		const processList = runScript(ns, serverList, 'server-hack.js', 1, hostname);
 		if (processList.length > 0) {
 			ns.print(`Running server-hack.js on ${hostname} with PID(s):`);
 			ns.print(...processList.map((process) => process.pid));
 		}
 	}
-    */
-
-	//run server-hack.js on 'n00dles'
-	const processList = runScript(ns, serverList, 'server-hack.js', 1, 'n00dles');
-	if (processList.length > 0) {
-		ns.print('Running server-hack.js on n00dles with PID:');
-		ns.print(...processList.map((process) => process.pid));
-	}
-
-	ns.ui.openTail();
+	*/
+    
+	ns.tprint('Setup complete');
 }
